5de5903083b098bcb0b836bb0af76cf32b3ab9dc
marce <marcelopickler01@gmail.com>
2019-08-12 22:02:45 -0300

    commit

---
+++ Downloads/spring-tool-suite-3.9.5.RELEASE-e4.8.0-win32-x86_64/sts-bundle/sts-3.9.5.RELEASE/stacks-implementations-project/src/br/univille/estd/stacks/array/ArrayStack.java
@@ -0,0 +1,64 @@
+package br.univille.estd.stacks.array;
+
+import br.univille.estd.stacks.EmptyStackException;
+import br.univille.estd.stacks.Stack;
+
+public class ArrayStack<E extends Comparable> implements Stack<E>{
+	
+	protected int capacity;
+	public static final int CAPACITY = 1000;
+	protected E S[];
+	protected int top = -1;
+	protected E save;
+	public ArrayStack() {
+		this(CAPACITY);
+	}
+	public ArrayStack(int capacity) {
+		this.capacity = capacity;
+		S = (E[])new Object[this.capacity];
+	}
+	@Override
+	public int size() {
+		return top+1;
+	}
+	@Override
+	public boolean isEmpty() {
+		return (top<0);
+	}
+	@Override
+	public E top() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException("pilha vazia");
+		}
+		return S[top];
+	}
+	@Override
+	public void push(E element) {
+		if(size() == capacity) {
+			throw new FullStackException("pilha cheia");
+		}
+		top = top + 1;    //Print top printa o índice
+		S[top] = element; //Print S[top] printa o elemento
+	}
+	@Override
+	public E pop() throws EmptyStackException {
+		// TODO Auto-generated method stub
+		if (isEmpty() == true) {
+			throw new EmptyStackException("pilha vazia");
+		}
+		E e = S[top];
+		S[top] = null;
+		top = top - 1;
+		
+		return e;
+	}
+
+	public int min(E element,E topo){
+		//save = S[top];
+		return topo.compareTo(element);
+		//1 variavel e 2 arranjos
+		//o push vai ser para os dois arranjos, e o pop apenas em um
+		//no final o arranjo vazio sera igual ao cheio
+	}
+
+}
---
+++ Downloads/spring-tool-suite-3.9.5.RELEASE-e4.8.0-win32-x86_64/sts-bundle/sts-3.9.5.RELEASE/stacks-implementations-project/src/br/univille/estd/stacks/array/Main.java
@@ -0,0 +1,10 @@
+package br.univille.estd.stacks.array;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
